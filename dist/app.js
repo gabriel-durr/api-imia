2b93c5220ce6a3ce4aa6551360b353214b3bfae0
Gabriel Dürr <gabriel.durr@outlook.com>
2022-10-13 15:35:56 -0300

    :bug: fixbug CORS Header Fetch API

--- dist/app.js
+++ dist/app.js
@@ -2,6 +2,7 @@
 var _datajson = require('./data/data.json'); var _datajson2 = _interopRequireDefault(_datajson);
 var _dataxjson = require('./data/datax.json'); var _dataxjson2 = _interopRequireDefault(_dataxjson);
 var _RadarChartUtils = require('./utils/RadarChartUtils');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
 
 const app = _express2.default.call(void 0, );
 
@@ -10,6 +11,19 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log("Server Runing 🛅" + PORT);
 });
+
+// CORS Headers
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+	res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	app.use(_cors2.default.call(void 0, ));
+	next();
+});
+
+// Manipulation
 
 app.get("/", (req, res) => {
 	var labels = _RadarChartUtils.getLabels.call(void 0, _dataxjson2.default);
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import data from "./data/data.json" assert {type: "json"};
 import datax from "./data/datax.json" assert {type: "json"};
 import {Unpack, getLabels, generateGraph} from "./utils/RadarChartUtils";
+import cors from "cors";
 
 const app = express();
 
@@ -10,6 +11,19 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log("Server Runing 🛅" + PORT);
 });
+
+// CORS Headers
+
+app.use((req, res, next) => {
+	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+	res.header("Access-Control-Allow-Origin", "*");
+	//Quais são os métodos que a conexão pode realizar na API
+	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	app.use(cors());
+	next();
+});
+
+// Manipulation
 
 app.get("/", (req, res) => {
 	var labels = getLabels(datax);
